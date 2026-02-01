23c4c7ff7af50c90da10f221d01893e3ad8cf6d7
abdielsouza <abdielcsouza@gmail.com>
2026-02-01 15:35:10 -0300

    added API support for user contexts.

--- lib/skulldb/api.ex
+++ lib/skulldb/api.ex
@@ -7,33 +7,90 @@ defmodule Skulldb.API do
   alias Skulldb.Graph.Engine
   alias Skulldb.Graph.TransactionManager
   alias Skulldb.Graph
-  # alias Skulldb.{Context, Authorization, AuditLog}
+  alias Skulldb.{Context, Authorization, AuditLog}
 
-  # Node operations
-  def create_node(labels \\ [], props \\ []), do: Engine.create_node(labels, props)
-  def create_node(tx, labels, props), do: Graph.create_node(tx, labels, props)
+  # Node operations - with context support
+  def create_node(labels \\ [], props \\ []), do: create_node(Context.anonymous(), labels, props)
+  def create_node(tx, labels, props), do: create_node(Context.anonymous(), tx, labels, props)
+
+  def create_node(%Context{} = context, labels, props) when is_list(labels) do
+    with :ok <- Authorization.authorize(context, :create, :node) do
+      enhanced_props = maybe_add_tenant(props, context)
+      result = Engine.create_node(labels, enhanced_props)
+      AuditLog.log(context, :create_node, %{labels: labels, props: enhanced_props})
+      result
+    end
+  end
+
+  def create_node(%Context{} = context, tx, labels, props) do
+    with :ok <- Authorization.authorize(context, :create, :node) do
+      enhanced_props = maybe_add_tenant(props, context)
+      result = Graph.create_node(tx, labels, enhanced_props)
+      AuditLog.log(context, :create_node, %{labels: labels, props: enhanced_props})
+      result
+    end
+  end
 
   # Edge operations
   def create_edge(from_id, to_id, label, props \\ []), do: Engine.create_edge(from_id, to_id, label, props)
   def create_edge(tx, type, from, to, props), do: Graph.create_edge(tx, type, from, to, props)
 
-  # Query
-  def query(q), do: Skulldb.Query.read_from_string(q)
+  # Query - with context support
+  def query(%Context{} = context, query_string) do
+    with :ok <- Authorization.authorize(context, :query, :graph) do
+      result = Skulldb.Query.read_from_string(query_string, context)
+      AuditLog.log(context, :query, %{query: query_string})
+      result
+    end
+  end
+
+  def query(q), do: query(Context.anonymous(), q)
 
   # Transactions
   def begin_transaction, do: Skulldb.Graph.TxEngine.begin()
   def commit_transaction(tx), do: TransactionManager.commit(tx)
   def rollback_transaction(tx), do: TransactionManager.rollback(tx)
 
-  # Updates/Deletes
-  def update_node(tx, id, changes), do: Graph.update_node(tx, id, changes)
-  def delete_node(tx, id), do: Graph.delete_node(tx, id)
+  # Updates/Deletes - with context support
+  def update_node(%Context{} = context, tx, id, changes) do
+    with :ok <- Authorization.authorize(context, :update, :node, id) do
+      result = Graph.update_node(tx, id, changes)
+      AuditLog.log(context, :update_node, %{node_id: id, changes: changes})
+      result
+    end
+  end
+
+  def update_node(tx, id, changes), do: update_node(Context.anonymous(), tx, id, changes)
+
+  def delete_node(%Context{} = context, tx, id) do
+    with :ok <- Authorization.authorize(context, :delete, :node, id) do
+      result = Graph.delete_node(tx, id)
+      AuditLog.log(context, :delete_node, %{node_id: id})
+      result
+    end
+  end
+
+  def delete_node(tx, id), do: delete_node(Context.anonymous(), tx, id)
   def delete_edge(tx, id), do: Graph.delete_edge(tx, id)
 
-  # Reads
-  def all_nodes, do: Graph.all_nodes()
+  # Reads - with context support
+  def all_nodes(%Context{} = context) do
+    with :ok <- Authorization.authorize(context, :read, :node) do
+      nodes = Graph.all_nodes()
+      filter_by_tenant(nodes, context)
+    end
+  end
+
+  def all_nodes, do: all_nodes(Context.anonymous())
   def all_edges, do: Graph.all_edges()
-  def get_node(node_id), do: Graph.get_node(node_id)
+
+  def get_node(%Context{} = context, node_id) do
+    with :ok <- Authorization.authorize(context, :read, :node, node_id) do
+      Graph.get_node(node_id)
+    end
+  end
+
+  def get_node(node_id), do: get_node(Context.anonymous(), node_id)
   def get_edge(edge_id), do: Graph.get_edge(edge_id)
 
   # Utilities
@@ -59,4 +116,24 @@ defmodule Skulldb.API do
   def node_exists?(node_id), do: Graph.node_exists?(node_id)
   def edge_exists?(edge_id), do: Graph.edge_exists?(edge_id)
   def neighbors(node_id), do: Graph.neighbors(node_id)
+
+  # Private helper functions
+  defp maybe_add_tenant(props, %Context{tenant_id: nil}), do: props
+
+  defp maybe_add_tenant(props, %Context{tenant_id: tenant_id}) do
+    Keyword.put(props, :tenant_id, tenant_id)
+  end
+
+  defp filter_by_tenant(nodes, %Context{tenant_id: nil}), do: nodes
+
+  defp filter_by_tenant(nodes, %Context{tenant_id: tenant_id, roles: roles}) do
+    if :admin in roles do
+      nodes
+    else
+      Enum.filter(nodes, fn node ->
+        node_tenant = Map.get(node.properties, :tenant_id)
+        is_nil(node_tenant) or node_tenant == tenant_id
+      end)
+    end
+  end
 end
